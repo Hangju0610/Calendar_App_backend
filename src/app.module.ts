f3b1e4d661c8f5e0086ff8a092e434db106dfb8d
hangju0610 <phj950610@naver.com>
2024-01-06 17:01:47 +0900

    Feat: Add typeORM Module to appModule TypeORM 모듈 사용을 위해 appModule에 기능 추가 진행

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,31 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => {
+        return {
+          type: 'postgres',
+          host: configService.get('DB_HOST'),
+          port: Number(configService.get('DB_PORT')),
+          database: configService.get('DB_DATABASE'),
+          username: configService.get('DB_USERNAME'),
+          password: configService.get('DB_PASSWORD'),
+          autoLoadEntities: true,
+          synchronize: false,
+          logging: true, // Logging 기능은 추후 끄는 걸로
+        };
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

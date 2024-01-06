66f9ef46ce3959848d918ba84850cb2158829cd9
hangju0610 <phj950610@naver.com>
2024-01-06 17:38:46 +0900

    Feat : Add UserEntity

---
+++ src/user/entity/user.entity.ts
@@ -0,0 +1,28 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ unique: true, nullable: false })
+  email: string;
+
+  @Column({ nullable: true })
+  password: string;
+
+  @Column({ nullable: false, unique: true })
+  nickname: string;
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ name: 'updated_at' })
+  updatedAt: Date;
+}
